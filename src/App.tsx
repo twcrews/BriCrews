e8e94b410c1f7627f87df952419b4c951dbe14c2
Tommy Crews <twcrews96@gmail.com>
2021-07-26 16:54:23 -0500

    Added more content.

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,19 @@ import Data from './Data.json'
 function App() {
   return (
     <Material.Container 
-      disableGutters
       maxWidth="lg"
     >
       <Material.Grid
         container
         alignItems="flex-start"
         spacing={2}
-        style={{ padding: "40px 20px"}}
+        style={{ padding: "40px 0"}}
       >
         <Material.Grid
           item
           xs={12}
-          md={4}
-          lg={3}
+          md={5}
+          lg={4}
         >
           <Material.Paper 
             style={{ 
@@ -42,16 +41,59 @@ function App() {
               variant="h3"
               variantMapping={{ h3: "h1" }}
               style={{ fontWeight: "bold" }}
+              paragraph
             >
               {Data.Content.Owner}
             </Material.Typography>
+            <Material.Typography 
+              variant="h6"
+              style={{ color: "#888" }}
+            >
+              {Data.Content.Title}
+            </Material.Typography>
+            <hr style={{ 
+              width: "182px",
+              margin: "30px auto",
+              border: "1px solid #ccc"
+            }} />
+            <Material.Grid
+              container
+              alignItems="center"
+              justifyContent="center"
+              spacing={4}
+            >
+            <Material.Grid item>
+              <Material.IconButton
+                href={Data.Links.LinkedIn}
+                target="_blank"
+                style={{ 
+                  color: "white",
+                  backgroundColor: "#0077b5" 
+                }}
+              >
+                <Icon.LinkedIn fontSize="large" />
+              </Material.IconButton>
+            </Material.Grid>
+              <Material.Grid item>
+                <Material.IconButton
+                  href={Data.Links.Facebook}
+                  target="_blank"
+                  style={{ 
+                    color: "white",
+                    backgroundColor: "#4267b2" 
+                  }}
+                >
+                  <Icon.Facebook fontSize="large" />
+                </Material.IconButton>
+              </Material.Grid>
+            </Material.Grid>
           </Material.Paper>
         </Material.Grid>
         <Material.Grid
           item
           xs={12}
-          md={8}
-          lg={9}
+          md={7}
+          lg={8}
         >
           <Material.Paper 
             style={{ 
