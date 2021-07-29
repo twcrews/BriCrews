e64a5142d4f02520993cdbf77365193c67d5957d
Tommy Crews <twcrews96@gmail.com>
2021-07-28 22:31:27 -0500

    Major content and style additions.

--- src/App.tsx
+++ src/App.tsx
@@ -5,114 +5,184 @@ import Data from './Data.json'
 
 function App() {
   return (
-    <Material.Container 
-      maxWidth="lg"
+    <div
+    style={{ 
+      backgroundImage: `url(${Data.Images.Background.Source})`,
+      minHeight: "100vh"
+    }}
     >
-      <Material.Grid
-        container
-        alignItems="flex-start"
-        spacing={2}
-        style={{ padding: "40px 0"}}
+      <Material.Container 
+        maxWidth="lg"
       >
         <Material.Grid
-          item
-          xs={12}
-          md={5}
-          lg={4}
+          container
+          alignItems="flex-start"
+          spacing={2}
+          style={{ padding: "40px 0"}}
         >
-          <Material.Paper 
-            style={{ 
-              borderRadius: "20px",
-              padding: "30px",
-              textAlign: "center"
-            }}
-            elevation={0}
+          <Material.Grid
+            item
+            xs={12}
+            md={5}
+            lg={4}
           >
-            <img
-              style={{
-                width: "100%",
-                maxWidth: "300px",
-                borderRadius: "10px"
+            <Material.Paper 
+              style={{ 
+                borderRadius: "20px",
+                padding: "30px",
+                textAlign: "center"
               }}
-              src={Data.Images.Portrait}
-              alt="Professional portrait"
-            />
-            <Material.Typography
-              variant="h3"
-              variantMapping={{ h3: "h1" }}
-              style={{ fontWeight: "bold" }}
-              paragraph
+              elevation={0}
             >
-              {Data.Content.Owner}
-            </Material.Typography>
-            <Material.Typography 
-              variant="h6"
-              style={{ color: "#888" }}
-            >
-              {Data.Content.Title}
-            </Material.Typography>
-            <hr style={{ 
-              width: "182px",
-              margin: "30px auto",
-              border: "1px solid #ccc"
-            }} />
-            <Material.Grid
-              container
-              alignItems="center"
-              justifyContent="center"
-              spacing={4}
-            >
-            <Material.Grid item>
-              <Material.IconButton
-                href={Data.Links.LinkedIn}
-                target="_blank"
-                style={{ 
-                  color: "white",
-                  backgroundColor: "#0077b5" 
+              <img
+                style={{
+                  width: "100%",
+                  maxWidth: "300px",
+                  borderRadius: "10px"
                 }}
+                src={Data.Images.Portrait.Source}
+                alt={Data.Images.Portrait.Alt}
+              />
+              <Material.Typography
+                variant="h3"
+                variantMapping={{ h3: "h1" }}
+                style={{ fontWeight: "bold" }}
+                paragraph
               >
-                <Icon.LinkedIn fontSize="large" />
-              </Material.IconButton>
-            </Material.Grid>
+                {Data.Content.Owner}
+              </Material.Typography>
+              <Material.Typography 
+                variant="h6"
+                style={{ color: "#888" }}
+              >
+                {Data.Content.Title}
+              </Material.Typography>
+              <Material.Divider 
+                variant="middle" 
+                style={{ margin: "20px 0" }}
+              />
+              <Material.Grid
+                container
+                alignItems="center"
+                justifyContent="center"
+                spacing={8}
+              >
               <Material.Grid item>
                 <Material.IconButton
-                  href={Data.Links.Facebook}
+                  href={Data.Links.LinkedIn}
                   target="_blank"
                   style={{ 
                     color: "white",
-                    backgroundColor: "#4267b2" 
+                    backgroundColor: "#0077b5" 
                   }}
                 >
-                  <Icon.Facebook fontSize="large" />
+                  <Icon.LinkedIn fontSize="large" />
                 </Material.IconButton>
               </Material.Grid>
-            </Material.Grid>
-          </Material.Paper>
-        </Material.Grid>
-        <Material.Grid
-          item
-          xs={12}
-          md={7}
-          lg={8}
-        >
-          <Material.Paper 
-            style={{ 
-              borderRadius: "20px",
-              padding: "30px"
-            }}
-            elevation={0}
+                <Material.Grid item>
+                  <Material.IconButton
+                    href={Data.Links.Facebook}
+                    target="_blank"
+                    style={{ 
+                      color: "white",
+                      backgroundColor: "#4267b2" 
+                    }}
+                  >
+                    <Icon.Facebook fontSize="large" />
+                  </Material.IconButton>
+                </Material.Grid>
+              </Material.Grid>
+            </Material.Paper>
+          </Material.Grid>
+          <Material.Grid
+            item
+            xs={12}
+            md={7}
+            lg={8}
           >
-            <Material.Typography
-              variant="h4"
-              variantMapping={{ h4: "h2" }}
-              style={{ fontWeight: "bold" }}
+            <Material.Paper 
+              style={{ 
+                borderRadius: "20px",
+                padding: "30px"
+              }}
+              elevation={0}
             >
-              {Data.Content.Header}
-            </Material.Typography>
-          </Material.Paper>
+              <Material.Typography
+                variant="h4"
+                variantMapping={{ h4: "h2" }}
+                style={{ fontWeight: "bold" }}
+                paragraph
+              >
+                {Data.Content.Header}
+              </Material.Typography>
+              {Data.Content.Tagline.split("\n").map(line =>
+              <Material.Typography 
+                key={line.slice(0, 5)} 
+                paragraph
+              >
+                {line}
+              </Material.Typography>)}
+              <Material.Typography variant="h5">
+                {Data.Content.MembershipHeader}
+              </Material.Typography>
+              <Material.Grid container>
+                <Material.Grid
+                  item
+                  xs={12}
+                  md={7}
+                >
+                  <ul>
+                    {Data.Content.Memberships.map(membership =>
+                      <Material.Typography 
+                        paragraph
+                        key={membership.slice(0, 15)}
+                      >
+                        <li>{membership}</li>
+                      </Material.Typography>
+                    )}
+                  </ul>
+                </Material.Grid>
+                <Material.Grid
+                  item
+                  xs={12}
+                  md={5}
+                  style={{ textAlign: "center" }}
+                >
+                  <img
+                    src={Data.Images.MembershipLogo.Source}
+                    alt={Data.Images.MembershipLogo.Alt}
+                    style={{ 
+                      width: "100%",
+                      maxWidth: "300px" 
+                    }}
+                  />
+                </Material.Grid>
+              </Material.Grid>
+              <Material.Typography
+                variant="h5"
+                style={{ paddingTop: "20px" }}
+              >
+                {Data.Content.AwardHeader}
+              </Material.Typography>
+              <Material.List>
+                {Data.Content.Awards.map(award =>
+                  <Material.ListItem key={award.Label.slice(0, 15)}>
+                    <Material.ListItemIcon>
+                      <Material.Icon>
+                        {award.Icon}
+                      </Material.Icon>
+                    </Material.ListItemIcon>
+                    <Material.ListItemText>
+                      {award.Label}
+                    </Material.ListItemText>
+                  </Material.ListItem>
+                )}
+              </Material.List>
+            </Material.Paper>
+          </Material.Grid>
         </Material.Grid>
-      </Material.Grid>
-    </Material.Container>
+      </Material.Container>
+    </div>
   );
 }
 
