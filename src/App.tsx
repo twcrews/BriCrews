14251ed162e7bb96c72a627cf8733bfe96e0787e
Tommy Crews <twcrews96@gmail.com>
2021-07-29 22:31:49 -0500

    Added additional content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as Material from '@material-ui/core';
 import * as Icon from '@material-ui/icons';
-import Data from './Data.json'
+import Data from './Data.json';
 
 function App() {
   return (
@@ -30,7 +30,8 @@ function App() {
               style={{ 
                 borderRadius: "20px",
                 padding: "30px",
-                textAlign: "center"
+                textAlign: "center",
+                position: "sticky"
               }}
               elevation={0}
             >
@@ -116,12 +117,12 @@ function App() {
                 {Data.Content.Header}
               </Material.Typography>
               {Data.Content.Tagline.split("\n").map(line =>
-              <Material.Typography 
-                key={line.slice(0, 5)} 
-                paragraph
-              >
-                {line}
-              </Material.Typography>)}
+                <Material.Typography 
+                  key={line.slice(0, 5)} 
+                  paragraph
+                >
+                  {line}
+                </Material.Typography>)}
               <Material.Typography variant="h5">
                 {Data.Content.MembershipHeader}
               </Material.Typography>
@@ -178,6 +179,34 @@ function App() {
                   </Material.ListItem>
                 )}
               </Material.List>
+              <Material.Typography
+                variant="h5"
+                style={{ paddingTop: "20px" }}
+              >
+                {Data.Content.CertificationHeader}
+              </Material.Typography>
+              <ul>
+                {Data.Content.Certifications.map(Certificate => 
+                  <Material.Typography paragraph>
+                    <li>{Certificate}</li>
+                  </Material.Typography>
+                )}
+              </ul>
+              <Material.Divider 
+                variant="middle"
+                style={{ margin: "20px 0" }}
+              />
+              <Material.Typography
+                paragraph
+                variant="h4"
+                variantMapping={{ h4: "h2" }}
+                style={{ fontWeight: "bold" }}
+              >
+                {Data.Content.ExperienceHeader}
+              </Material.Typography>
+              <Material.Typography>
+                {Data.Content.ExperienceTagline}
+              </Material.Typography>
             </Material.Paper>
           </Material.Grid>
         </Material.Grid>
