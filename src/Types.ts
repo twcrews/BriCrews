958b5c1f6bf996da4965e4aed74ee81d656b98be
Tommy Crews <twcrews96@gmail.com>
2021-11-07 08:41:41 -0600

    Refined types.

--- src/Types.ts
+++ src/Types.ts
@@ -1,38 +1,59 @@
+import React from "react";
+import { ButtonProps } from "@material-ui/core";
+
 export type Body = {
-	owner: string,
-	title: string,
-	header: string,
-	tagline: string,
-	sections: Section[]
+	owner: string;
+	title: string;
+	header: string;
+	tagline: string;
+	sections: Section[];
 };
 
 export type Section = {
-	id: string,
-	type: string,
-	title: string,
-	content: HeroContent[] | PanelContent[] | TextContent[] | TileContent[] | ContactContent[] | FooterContent[]
+	id: string;
+	type: string;
+	title: string;
+	content:
+		| HeroContent[]
+		| PanelContent[]
+		| TextContent[]
+		| TileContent[]
+		| ContactContent[]
+		| FooterContent[];
 };
 
 export type HeroContent = {
-	subtitle: string,
-	buttons: ButtonContent[],
-	image: string
+	subtitle?: string;
+	buttons?: ButtonContent[];
+	image?: ImageContent;
+};
+
+export type ButtonContent = {
+	buttonProps?: ButtonProps;
+	anchor?: string;
+};
+
+export type ImageContent = {
+	source?: string;
+	alt?: string;
+	containerStyle?: React.CSSProperties;
+	imageStyle?: React.CSSProperties;
 };
 
 export type PanelContent = {
-	title: string,
-	description: string,
-	image: string
+	title?: string;
+	description?: string;
+	image?: ImageContent;
 };
 
 export type TextContent = {
-	title: string,
-	paragraphs: ParagraphContent[]
+	title?: string;
+	paragraphs?: ParagraphContent[];
 };
 
 export type TileContent = {
-	title: string,
-	description: string
+	title?: string;
+	description?: string;
 };
 
 export type ContactContent = {
@@ -43,13 +64,11 @@ export type FooterContent = {
 	// TODO
 };
 
-export type ButtonContent = {
-	label: string,
-	variant: string,
-	anchor: string
-};
-
 export type ParagraphContent = {
-	heading: string,
-	text: string
-}
+	heading?: string;
+	text?: string;
+	topPhoto?: ImageContent;
+	bottomPhoto?: ImageContent;
+	leftPhoto?: ImageContent;
+	rightPhoto?: ImageContent;
+};
