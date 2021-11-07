47feb7e872e0e32337f38a1834d19620dc835fbe
Tommy Crews <twcrews96@gmail.com>
2021-11-07 10:58:54 -0600

    Just at a stopping point

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,6 +1,16 @@
+import { Box } from "@mui/system";
+
 export type SectionProps = {
-	children: React.ReactNode;
 	id?: string;
 	title?: string;
-	type?: "hero" | "panel" | "tile" | "text" | "contact";
+	children?: React.ReactNode;
 };
+
+export function Section({children, id, title}: SectionProps) {
+	return (
+		<Box>
+			<h2 id={id}>{title}</h2>
+			{children}
+		</Box>
+	);
+}
